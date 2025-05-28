0990ed4eba8ac5d04cb5d46f2d699a47346828e0
JorgeReyes2023 <jorge.reyes@espoch.edu.ec>
2025-05-28 13:59:48 -0500

    mejoramiento de pagina de login

--- frontend/src/components/login/login.component.ts
+++ frontend/src/components/login/login.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MyTestService } from '../../services/my-test.service'; // Import your service if needed
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
 
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
