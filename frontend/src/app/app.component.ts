7a6158ce07d8286f206de902c8df10e2f19d1110
JorgeReyes2023 <jorge.reyes@espoch.edu.ec>
2025-05-28 13:44:06 -0500

    conexion front con back

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyTestService } from '../services/my-test.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private mytest: MyTestService) {}
   title = 'frontend';
 }
--- frontend/src/components/login/login.component.ts
+++ frontend/src/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MyTestService } from '../../services/my-test.service'; // Import your service if needed
+
+
 
 @Component({
   standalone: true,
@@ -14,10 +17,21 @@ export class LoginComponent {
   email = '';
   password = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private testService: MyTestService) {}
 
   login() {
+    this.testService.getData('').subscribe({
+      next: (res: any) => {
+        console.log('Response from API:', res);
+      },
+      error: (err: any) => {
+        console.error('Error fetching data:', err);
+      }
+    });
+
     console.log('Email:', this.email, 'Password:', this.password);
+   
     this.router.navigate(['/dashboard']);
+
   }
 }
