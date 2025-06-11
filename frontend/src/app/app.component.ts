a857f40ceea9c9704f1d6497bf59755f0df5fa1b
JorgeReyes2023 <jorge.reyes@espoch.edu.ec>
2025-06-11 14:33:06 -0500

    mensaje de alerta para campos de creacion de usario

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyTestService } from '../services/my-test.service';
+import { AlertComponent } from './_alert/alert.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AlertComponent,MatToolbarModule, MatButtonModule, MatCardModule, MatTabsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(private mytest: MyTestService) {}
---
+++ frontend/src/components/users/users.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';  
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { AlertService } from '../../app/_alert/alert.service';
+
+@Component({
+  selector: 'app-users',
+  imports: [CommonModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatSelectModule],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+
+  username = '';
+  email = '';
+  password = '';
+  role = '';
+
+  roles = [
+    { value: 'admin', viewValue: 'Administrador' },
+    { value: 'user', viewValue: 'Usuario' },
+  ];
+  constructor(private router: Router, private authService: AuthService, private alertService: AlertService) {}
+  
+  createUser() {
+    const userData = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      role: this.role
+    };
+    console.log('User Data:', userData);
+
+    if (!userData.username || !userData.email || !userData.password || !userData.role) {
+      console.error('All fields are required.');
+      this.alertService.error('Se requiere completar todos los campos.');
+      return;
+    }
+
+    
+
+    console.log('User Data:', userData);
+  }
+}
