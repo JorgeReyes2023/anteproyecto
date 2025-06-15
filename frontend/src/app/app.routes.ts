7bf7c2fbfc867e93f061577102919390fcd8659a
Jorge-2 <jorge.reyes@espoch.edu.ec>
2025-06-15 17:03:54 -0500

    add: path for dashboard compoent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { UsersComponent } from '../components/users/users.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'users', component: UsersComponent },
+    { path: 'dashboard', component: DashboardComponent },
     { path: '', redirectTo: '/users', pathMatch: 'full' },
 ];
