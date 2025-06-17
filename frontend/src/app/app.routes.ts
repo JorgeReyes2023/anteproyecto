2e6d2fc05b2f51d95edc15687fe96c266e64bea6
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-17 11:10:34 -0500

    add: route in app and navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,9 +5,16 @@ import { createUserComponent } from '../components/create-user/create-user.compo
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { UserInfoComponent } from '../components/user-info/user-info.component';
 import { UsersDataComponent } from '../components/users-data/users-data.component';
+import { NodesDataComponent } from '../components/nodes-data/nodes-data.component';
+import { CreateNodeComponent } from '../components/create-node/create-node.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  {
+    path: 'users',
+    component: UsersDataComponent,
+    canActivate: [roleGuard(['admin'])],
+  },
   {
     path: 'users/create',
     component: createUserComponent,
@@ -24,8 +31,13 @@ export const routes: Routes = [
     canActivate: [roleGuard(['user', 'admin'])],
   },
   {
-    path: 'users',
-    component: UsersDataComponent,
+    path: 'nodes',
+    component: NodesDataComponent,
+    canActivate: [roleGuard(['admin'])],
+  },
+  {
+    path: 'nodes/create',
+    component: CreateNodeComponent,
     canActivate: [roleGuard(['admin'])],
   },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
