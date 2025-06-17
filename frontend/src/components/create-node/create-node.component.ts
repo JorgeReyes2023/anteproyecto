f73914e2995e3820da5ccb7f7b2f79ff6c2da1bb
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-17 11:10:10 -0500

    create base for nodes data and create node

---
+++ frontend/src/components/create-node/create-node.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-create-node',
+  imports: [],
+  templateUrl: './create-node.component.html',
+  styleUrls: ['./create-node.component.css'],
+})
+export class CreateNodeComponent {}
---
+++ frontend/src/components/nodes-data/nodes-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nodes-data',
+  imports: [],
+  templateUrl: './nodes-data.component.html',
+  styleUrl: './nodes-data.component.css'
+})
+export class NodesDataComponent {
+
+}
