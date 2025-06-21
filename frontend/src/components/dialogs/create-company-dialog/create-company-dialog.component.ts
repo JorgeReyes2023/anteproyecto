32bdaf7798d485b4111606128c8562f24fb3074b
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-20 20:15:26 -0500

    add: dialog create company

--- frontend/src/components/dialogs/create-company-dialog/create-company-dialog.component.ts
+++ frontend/src/components/dialogs/create-company-dialog/create-company-dialog.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { Company } from '../../../models/company';
 
 @Component({
   selector: 'app-create-company-dialog',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
   templateUrl: './create-company-dialog.component.html',
-  styleUrl: './create-company-dialog.component.css'
+  styleUrl: './create-company-dialog.component.css',
 })
 export class CreateCompanyDialogComponent {
+  localCompany: Company;
 
+  constructor(
+    public dialogRef: MatDialogRef<CreateCompanyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public company: Company
+  ) {
+    // shallow copy – use structuredClone for deep copy if nested objects exist
+    this.localCompany = { ...company };
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.dialogRef.close(this.localCompany);
+    }
+  }
 }
