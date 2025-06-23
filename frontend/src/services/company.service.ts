60373c96aec190f3035c82cec0ae523ae254b717
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-23 11:56:47 -0500

    bf: fetch companies ok

---
+++ frontend/src/services/company.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { GeneralService } from './general.service';
+import { Company } from '../models/company';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompanyService {
+  constructor(private gService: GeneralService) {}
+
+  getCompanies(): any {
+    return this.gService.getData('companies');
+  }
+
+  createCompany(company: Company) {
+    return this.gService.postData('companies', company);
+  }
+
+  updateCompany(company: Company) {
+    return this.gService.putData(`companies/${company.id}`, company);
+  }
+
+  deleteCompany(companyId: number) {
+    return this.gService.deleteData(`companies/${companyId}`);
+  }
+}
