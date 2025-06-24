a9b04c8115311f7cc52ea7b5bdd6c6e50e297dc2
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-23 21:39:12 -0500

    create component projects data

---
+++ frontend/src/services/project.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { GeneralService } from './general.service';
+import { Project } from '../models/project';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  constructor(private gService: GeneralService) {}
+
+  getProjects(): Observable<Project[]> {
+    return this.gService.getData('projects');
+  }
+
+  getProjectById(id: number | string) {
+    return this.gService.getData(`projects/${id}`);
+  }
+
+  createProject(project: Project) {
+    return this.gService.postData('projects', project);
+  }
+
+  updateProject(id: number | string, project: Project) {
+    return this.gService.putData(`projects/${id}`, project);
+  }
+
+  deleteProject(id: number) {
+    return this.gService.deleteData(`projects/${id}`);
+  }
+}
