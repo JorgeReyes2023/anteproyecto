572f0e3d4a1180c532df57d2e9fa7b6ff10b0f9a
Jorge <jorge.reyes@espoch.edu.ec>
2025-06-23 20:54:47 -0500

    create projects data

---
+++ frontend/src/components/projects-data/projects-data.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-data',
+  imports: [],
+  templateUrl: './projects-data.component.html',
+  styleUrls: ['./projects-data.component.css'],
+})
+export class ProjectsDataComponent {}
